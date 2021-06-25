4c969fa5f2f995bf19a811ca6fa9f6318e18b8f5
danyingwind <2445232157@qq.com>
2021-06-25 21:25:14 +0800

    学习datasets.py，有问题未解决

--- datasets.py
+++ datasets.py
@@ -5,30 +5,32 @@ import numpy as np
 from torch.utils.data import Dataset#用于实现数据读取的工具包
 
 
-class TrainDataset(Dataset):
+class TrainDataset(Dataset):# 训练集的处理
     def __init__(self, h5_file): # h5_file是指HDF文件
         super(TrainDataset, self).__init__()# super()与调用父类的函数有关，表示采用父类的__init__()函数
         self.h5_file = h5_file#初始化变量
 
     def __getitem__(self, idx):
         with h5py.File(self.h5_file, 'r') as f:#读取文件
-            #np.expand_dims(a, axis=0)表示在0位置添加数据，数组(1,2,3)一共有0、1、2、3四个位置可以添加数据。
+            #np.expand_dims(a, axis=0)表示在多维数组0维，也就是最外围，添加数据，数组(1,2,3)一共有0、1、2、3四个维度可以添加数据；0维度就是直接给原来数组外面加括号
             return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)
 
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as f:
+            #返回的是元素的个数
             return len(f['lr'])
 
 
-class EvalDataset(Dataset):
+class EvalDataset(Dataset): # 验证集的处理
     def __init__(self, h5_file):
         super(EvalDataset, self).__init__()# super()与调用父类的函数有关，表示采用父类的__init__()函数
         self.h5_file = h5_file#初始化变量
 
     def __getitem__(self, idx):#这里和train不太一样
         with h5py.File(self.h5_file, 'r') as f:
+            # 这里是不是说数据的维度不一样呢？
             return np.expand_dims(f['lr'][str(idx)][:, :] / 255., 0), np.expand_dims(f['hr'][str(idx)][:, :] / 255., 0)
 
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as f:
-            return len(f['lr'])
+            return len(f['lr']) # 返回元素个数
