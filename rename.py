9982930eaf45681397484ee1b488b2fe88efc3a9
danyingwind <2445232157@qq.com>
2021-06-27 19:39:56 +0800

    文件重命名

---
+++ rename.py
@@ -0,0 +1,23 @@
+import os
+ 
+import re
+ 
+filepath = "/home/wangdanying/SRCNN/R1TexturePng"
+
+#要删除的名字字符串 
+delect = "8bit_p420_"
+if __name__ == '__main__':
+    if not os.path.exists(filepath):
+        print("目录不存在!!")
+        os._exit(1)
+    filenames = os.listdir(filepath)
+    print("文件数目为%i" %len(filenames))
+    count = 0
+    for name in filenames:
+        newname = re.sub(delect, '', name)
+        os.rename(filepath + '/' + name, filepath + '/' + newname)
+        count += 1
+        if count % 100 == 0:
+            print("第%i个文件已经改名完成" %count)
+ 
+
