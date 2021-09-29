883a8d7b200ecc7e28a464f6a43f410c3c88d887
danyingwind <2445232157@qq.com>
2021-09-29 10:16:44 +0800

    prepare_tex_occ.py代码优化

--- prepare_occ_tex.py
+++ prepare_occ_tex.py
@@ -13,10 +13,6 @@ def train(args):
     # https://blog.csdn.net/qq_34859482/article/details/80115237
     # h5py是1个管理数据的容器，里面可以有具体的数据以及数据分组
 
-    lr_tex_patches = []
-    hr_tex_patches = []
-    lr_occ_patches = []
-    hr_occ_patches = []
     lr_patches = []
     hr_patches = []
 
@@ -80,25 +76,38 @@ def eval(args):
     lr_group = h5_file.create_group('lr')
     hr_group = h5_file.create_group('hr')
 
+    # 获取四类图像的路径
+    hr_tex_paths = sorted(glob.glob('{}/*'.format(args.hr_tex_images)));
+    lr_tex_paths = sorted(glob.glob('{}/*'.format(args.lr_tex_images)));
+    hr_occ_paths = sorted(glob.glob('{}/*'.format(args.hr_occ_images)));
+    lr_occ_paths = sorted(glob.glob('{}/*'.format(args.lr_occ_images)));
+
     # enumerate是枚举函数，i、hr_path分别对应索引下标和路径
-    for i, hr_path in enumerate(sorted(glob.glob('{}/*'.format(args.images_hr)))):
-        hr = pil_image.open(hr_path).convert('RGB')
-        lr_path = hr_path[0:31]+'l'+hr_path[32:] # 获取lr路径
-        lr = pil_image.open(lr_path).convert('RGB') # 读取lr文件
+    for i in range(0,len(hr_tex_paths)) : 
+        hr_tex = pil_image.open(hr_tex_paths[i]).convert('RGB') # 读取hr_tex文件
+        lr_tex = pil_image.open(lr_tex_paths[i]).convert('RGB') # 读取lr_tex文件
+        hr_occ = pil_image.open(hr_occ_paths[int(i/2)]).convert('RGB') # 读取hr_occ文件
+        lr_occ = pil_image.open(lr_occ_paths[int(i/2)]).convert('RGB') # 读取lr_occ文件
+
+        # 通过观察的中间文件可以发现，占用图的大小始终小于texture，
+        # 因此在处理的时候需要先将occupancy变得和texture一样大小
+        hr_occ = hr_occ.resize((hr_tex.width, hr_tex.height), resample=pil_image.BICUBIC)
+        lr_occ = lr_occ.resize((lr_tex.width, lr_tex.height), resample=pil_image.BICUBIC)
        
-        # # 调整图像尺寸
-        # hr_width = (hr.width // args.scale) * args.scale
-        # hr_height = (hr.height // args.scale) * args.scale
-        # # 获得低/高分辨率图像
-        # hr = hr.resize((hr_width, hr_height), resample=pil_image.BICUBIC)
-        # lr = hr.resize((hr_width // args.scale, hr_height // args.scale), resample=pil_image.BICUBIC)
-        # lr = lr.resize((lr.width * args.scale, lr.height * args.scale), resample=pil_image.BICUBIC)
-        # 数据格式转换
-        hr = np.array(hr).astype(np.float32)
-        lr = np.array(lr).astype(np.float32)
-        # 计算y分量
-        hr = convert_rgb_to_y(hr)
-        lr = convert_rgb_to_y(lr)
+        # 数据类型转换
+        hr_tex = np.array(hr_tex).astype(np.float32)
+        lr_tex = np.array(lr_tex).astype(np.float32)
+        hr_occ = np.array(hr_occ).astype(np.float32)
+        lr_occ = np.array(lr_occ).astype(np.float32)
+        # 根据RGB求Y
+        hr_tex = convert_rgb_to_y(hr_tex)
+        lr_tex = convert_rgb_to_y(lr_tex)
+        hr_occ = convert_rgb_to_y(hr_occ)
+        lr_occ = convert_rgb_to_y(lr_occ)
+
+        # 拼接occupancy和texture
+        lr = np.array([lr_tex,lr_occ])
+        hr = np.array([hr_tex,hr_occ])
 
         # 这里得到hr和lr的方式和大小都是和train一样，但是生成数据集的方式不一样
         # train是生成patch，然后用patch创建数据集，一个hr数据集，一个lr数据集
